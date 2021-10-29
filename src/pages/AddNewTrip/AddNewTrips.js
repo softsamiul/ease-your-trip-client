d85d24c86e72c2c48ed1fcd4396b18077425c53b
softsamiul <softsamiul@gmail.com>
2021-10-29 18:57:02 +0530

    manage trip, my trips, add new trip pages are added

---
+++ src/pages/AddNewTrip/AddNewTrips.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddNewTrips = () => {
+    return (
+        <div>
+            <h2>Add New Trip</h2>
+        </div>
+    );
+};
+
+export default AddNewTrips;
---
+++ src/pages/ManageAllTrips/ManageAllTrips.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageAllTrips = () => {
+    return (
+        <div>
+            <h2>Manage All Trips</h2>
+        </div>
+    );
+};
+
+export default ManageAllTrips;
