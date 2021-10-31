3f4bcf5cc0df5938bcddcd52f968064fd6952ff7
softsamiul <softsamiul@gmail.com>
2021-10-31 16:29:49 +0530

    process booking updated

--- src/pages/ProcessingBooking/ProcessingBooking.js
+++ src/pages/ProcessingBooking/ProcessingBooking.js
@@ -31,6 +31,7 @@ const ProcessingBooking = () => {
         .then(data=>setService(data))
     },[])
 
+    const {name, desc, _id, thumbnail, price, rating, days, persons} = service;
 
     return (
         <div>
@@ -40,26 +41,52 @@ const ProcessingBooking = () => {
                 <p>You are applying for {service.name}</p>
             </div>
             <div>
-            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center justify-center w-2/5 mx-auto mb-8">
-                {/* Fuull name */}
-                <input className="border-2 border-gray d-block py-1 px-2 w-full" defaultValue={user.displayName} {...register("fullName", { required: true })} readOnly/>
-                {/* Email */}
-                <input className="border-2 border-gray d-block py-1 px-2 w-full my-2" defaultValue={user.email} {...register("email", { required: true })} readOnly/>
-                {/* Trip package name */}
-                <input className="border-2 border-gray d-block py-1 px-2 w-full my-2" defaultValue={service.name} {...register("tripPackageName", { required: true })} readOnly/>
-                {/* Sending status */}
-                <input  style={{display:"none"}} defaultValue={"Pending"} {...register("status", { required: true })}/>
-                
-                {/* City */}
-                <input className="border-2 border-gray d-block py-1 px-2 w-full" placeholder="City" {...register("city", { required: true})} required/>
-                {/* Area pincode */}
-                <input className="border-2 border-gray d-block py-1 px-2 w-full mt-2" placeholder="Area pin code" type="number" {...register("phone")} required/>
-                {/* Address */}
-                <textarea className="border-2 border-gray d-block py-1 px-2 w-full my-2" placeholder="Address" {...register("address")} required/>
-                {/* Phone num */}
-                <input className="border-2 border-gray d-block py-1 px-2 w-full" placeholder="Phone" type="number" {...register("phone")} required/>
-                <input className="mt-2" type="submit" value="Confirm Booking" />
-            </form>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 w-11/12 md:w-10/12 mx-auto my-12 gap-4 ">
+                <div>
+                    <div className="p-4 single-service">
+                        <div>
+                            <img src={thumbnail} className="img-fluid rounded-lg" style={{height:"272px"}} alt="" />
+                        </div>
+                        <div class="pacakge-fix">
+                            <h2 className="text-2xl mt-2 font-base">{name}</h2>
+                            <div className="grid grid-cols-3 gap-3 py-1 my-1">
+                                <p><i className="fas fa-user-friends"></i> {persons} Persons</p>
+                                <p className="border-r border-l border-gray"><i className="fas fa-clock text-blue-900"></i> {days} Days</p>
+                                <p><i className="fas fa-star text-yellow-500"></i> {rating} Rating</p>
+                            </div>
+                            <div>
+                                <p className="text-justify">{desc}</p>
+                            </div>
+                            <p className="text-2xl font-bold py-2"><i className="fas fa-rupee-sign"></i> {price}</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center justify-center  mx-auto mb-8">
+                        <p className="text-xl font-medium my-3">Please fill the form to confirm order</p>
+                        {/* Fuull name */}
+                        <input className="border-2 border-gray d-block py-1 px-2 w-full" defaultValue={user?.displayName} {...register("fullName", { required: true })} readOnly/>
+                        {/* Email */}
+                        <input className="border-2 border-gray d-block py-1 px-2 w-full my-2" defaultValue={user.email} {...register("email", { required: true })} readOnly/>
+                        {/* Trip package name */}
+                        <input className="border-2 border-gray d-block py-1 px-2 w-full my-2" defaultValue={service?.name} {...register("tripPackageName", { required: true })} readOnly/>
+                        {/* Sending status */}
+                        <input  style={{display:"none"}} defaultValue={"Pending"} {...register("status", { required: true })}/>
+                        
+                        {/* City */}
+                        <input className="border-2 border-gray d-block py-1 px-2 w-full" placeholder="City" {...register("city", { required: true})} required/>
+                        {/* Area pincode */}
+                        <input className="border-2 border-gray d-block py-1 px-2 w-full mt-2" placeholder="Area pin code" type="number" {...register("phone")} required/>
+                        {/* Address */}
+                        <textarea className="border-2 border-gray d-block py-1 px-2 w-full my-2" placeholder="Address" {...register("address")} required/>
+                        {/* Phone num */}
+                        <input className="border-2 border-gray d-block py-1 px-2 w-full" placeholder="Phone" type="number" {...register("phone")} required/>
+                        <input className="bg-yellow-500 py-1 px-4 text-white font-bold mt-2" type="submit" value="Confirm Booking" />
+                    </form>
+                </div>
+            </div>
+            
             </div>
         </div>
     );
